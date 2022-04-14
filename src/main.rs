63d2366854c9955c49be6475e04081cde2d7e3aa
LIANGYU <n795113@gmail.com>
2022-04-14 14:13:57 +0800

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod args;
 use args::Args;
-use image::{ io::Reader, DynamicImage, ImageFormat, GenericImageView, imageops::FilterType::Triangle };
-use std::{ io::BufReader, fs::File };
+use image::{ io::Reader, DynamicImage, ImageFormat, GenericImageView, imageops::FilterType::Triangle, ImageError };
 
 #[derive(Debug)]
 enum ImageDataErrors {
     DifferentImageFormats,
-    BufferTooSmall
+    BufferTooSmall,
+    UnableToReadImageFromPath(std::io::Error),
+    UnableToFormatImage(String),
+    UnableToDecodeImage(ImageError),
+    UnableToSaveImage(ImageError)
 }
 
 struct FloatingImage {
@@ -38,8 +41,8 @@ impl FloatingImage {
 
 fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
-    let (image_1, image_format_1) = find_imgae_from_path(args.image_1);
-    let (image_2, image_format_2) = find_imgae_from_path(args.image_2);
+    let (image_1, image_format_1) = find_imgae_from_path(args.image_1)?;
+    let (image_2, image_format_2) = find_imgae_from_path(args.image_2)?;
 
     if image_format_1 != image_format_2 {
         return Err(ImageDataErrors::DifferentImageFormats);
@@ -51,22 +54,34 @@ fn main() -> Result<(), ImageDataErrors> {
     let combined_data = combine_images(image_1, image_2);
     output.set_data(combined_data)?;
 
-    image::save_buffer_with_format(
+    if let Err(e) = image::save_buffer_with_format(
         output.name, 
         &output.data, 
         output.width, 
         output.height, 
         image::ColorType::Rgba8, 
         image_format_1
-    ).unwrap();
-    Ok(())
+    ) {
+        Err(ImageDataErrors::UnableToSaveImage(e))
+    } else {
+        Ok(())
+    }
 }
 
-fn find_imgae_from_path(path: String) -> (DynamicImage, ImageFormat) {
-    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
-    let image_format: ImageFormat = image_reader.format().unwrap();
-    let image: DynamicImage = image_reader.decode().unwrap();
-    (image, image_format)
+fn find_imgae_from_path(path: String) -> Result<(DynamicImage, ImageFormat), ImageDataErrors> {
+    match Reader::open(&path) {
+        Ok(image_reader) => {
+            if let Some(image_format) = image_reader.format() {
+                match image_reader.decode() {
+                    Ok(image) => Ok((image, image_format)),
+                    Err(e) => Err(ImageDataErrors::UnableToDecodeImage(e))
+                }
+            } else {
+                return Err(ImageDataErrors::UnableToFormatImage(path));
+            }
+        },
+        Err(e) => Err(ImageDataErrors::UnableToReadImageFromPath(e))
+    }
 }
 
 fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
